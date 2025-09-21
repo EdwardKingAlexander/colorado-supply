6ea7fe44e7a898815a4d5ec78910c9412e0f0526
Edward <edward@rockymountainweb.design>
2025-09-21 08:08:17 -0600

    Fixed Inertia SSR import for ComonJS

--- bootstrap/ssr/ssr.js
+++ bootstrap/ssr/ssr.js
@@ -1,10 +1,10 @@
-import { createServer } from '@inertiajs/server'
+import pkg from '@inertiajs/server'
+const { createServer } = pkg
+
 import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, h } from 'vue'
-
-const { createServer } = pkg
 
 createServer((page) =>
   createInertiaApp({
