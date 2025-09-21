979b2e34340b4123ff474ea4ab1c9c0119b9daa6
Edward <edward@rockymountainweb.design>
2025-09-21 07:58:39 -0600

    Add Inertia SSR entrypoint

---
+++ bootstrap/ssr/ssr.js
@@ -0,0 +1,20 @@
+import { createServer } from '@inertiajs/server'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { renderToString } from '@vue/server-renderer'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createSSRApp, h } from 'vue'
+
+createServer((page) =>
+  createInertiaApp({
+    page,
+    render: renderToString,
+    resolve: (name) =>
+      resolvePageComponent(
+        `./resources/js/Pages/${name}.vue`,
+        import.meta.glob('./resources/js/Pages/**/*.vue')
+      ),
+    setup: ({ App, props, plugin }) => {
+      return createSSRApp({ render: () => h(App, props) }).use(plugin)
+    },
+  })
+)
