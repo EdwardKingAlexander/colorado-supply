32453e8de2843a70d1d6e6f1397398e729f497c1
Edward <edward@rockymountainweb.design>
2025-09-21 08:18:50 -0600

    Fixed Inertia SSR import for ComonJS

--- bootstrap/ssr/ssr.js
+++ bootstrap/ssr/ssr.js
@@ -1,10 +1,11 @@
-import pkg from '@inertiajs/server'
-const { createServer } = pkg
-
 import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, h } from 'vue'
+
+// IMPORTANT: @inertiajs/server is CommonJS.
+// On Node 22 ESM, grab the default export via top-level await.
+const { default: createServer } = await import('@inertiajs/server')
 
 createServer((page) =>
   createInertiaApp({
@@ -15,8 +16,7 @@ createServer((page) =>
         `./resources/js/Pages/${name}.vue`,
         import.meta.glob('./resources/js/Pages/**/*.vue')
       ),
-    setup: ({ App, props, plugin }) => {
-      return createSSRApp({ render: () => h(App, props) }).use(plugin)
-    },
+    setup: ({ App, props, plugin }) =>
+      createSSRApp({ render: () => h(App, props) }).use(plugin),
   })
 )
