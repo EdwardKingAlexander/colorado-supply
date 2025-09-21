02f5ee5655154f5130cdd25b7a059357a7322cc9
Edward <edward@rockymountainweb.design>
2025-09-21 08:50:19 -0600

    testingt o see if ssr works now

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,17 +1,29 @@
-import { createInertiaApp } from '@inertiajs/vue3'
 import createServer from '@inertiajs/server'
-import { createSSRApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createSSRApp, h } from 'vue'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy' // 👈 same as app.js
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createServer((page) =>
   createInertiaApp({
     page,
     render: renderToString,
     resolve: (name) =>
-      resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ App, props, plugin }) {
-      return createSSRApp({ render: () => h(App, props) }).use(plugin)
-    },
+      resolvePageComponent(
+        `./resources/js/Pages/${name}.vue`,
+        import.meta.glob('./resources/js/Pages/**/*.vue')
+      ),
+    setup: ({ App, props, plugin }) =>
+      createSSRApp({ render: () => h(App, props) })
+        .use(plugin)
+        .use(ZiggyVue) // 👈 include Ziggy if you use it in client too
+        .mixin({
+          methods: {
+            appName: () => appName,
+          },
+        }),
   })
 )
