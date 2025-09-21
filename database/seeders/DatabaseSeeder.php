8ad0ebd386fd775a2a6a467aa4e8c9e65805be91
Edward <edward@rockymountainweb.design>
2025-09-20 21:35:39 -0600

    modified Admin model to be authenticatable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Admin;
 use App\Models\Vendor;
 use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,14 +18,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //User::factory(10)->create();
+        User::factory(5)->create();
 
-        Artisan::call('make:filament-user',
-    [
-        '--name' => 'Edward',
-        '--email' => 'edward@rockymountainweb.design',
-        '--password' => 'password',
-    ]);
+    //     Artisan::call('make:filament-user',
+    // [
+    //     '--name' => 'Edward',
+    //     '--email' => 'edward@rockymountainweb.design',
+    //     '--password' => 'password',
+    // ]);
+
+      // Create a default admin
+        Admin::firstOrCreate(
+            ['email' => 'edward@rockymountainweb.design'],
+            [
+                'name' => 'Edward',
+                'password' => bcrypt('password'),
+            ]
+        );
 
         Vendor::factory()->count(10)->create();
         Category::factory()->count(5)->create();
